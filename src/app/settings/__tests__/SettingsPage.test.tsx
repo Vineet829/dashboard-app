c7ee339890d6cac84ed55edcb1b3f9ae27766916
Vineet829 <vineetsingh5987@gmail.com>
2024-08-10 16:36:45 +0530

    test: add settings component tests

--- src/app/settings/__tests__/SettingsPage.test.tsx
+++ src/app/settings/__tests__/SettingsPage.test.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import store from '../../../store/store';
 import Settings from '../page';
 
+
 describe('Settings Component', () => {
   test('renders Settings component', () => {
     render(
